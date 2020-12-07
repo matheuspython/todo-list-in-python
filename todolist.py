ffafb624e16adc9a2442bd333031232fe4ecea3b
matheus souza <ruivorobado@gmail.com>
2020-12-07 20:52:26 -0300

    task list start done

---
+++ todolist.py
@@ -0,0 +1,8 @@
+tasks = ['program', 'exercise myself','eat']#initial tasks placed on the list
+
+def fillList():
+    for i in tasks:
+        print(i)
+
+print("your tasks today are: ")
+fillList()
