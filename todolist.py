afd92db337659e50448b01250d51cbb2a38537d0
matheus souza <ruivorobado@gmail.com>
2020-12-07 23:00:29 -0300

    code that adds new items to the list works

--- todolist.py
+++ todolist.py
@@ -1,8 +1,31 @@
-tasks = ['program', 'exercise myself','eat']#initial tasks placed on the list
+tasks = ['program', 'exercise myself', 'eat']#initial tasks placed on the list
 
 def fillList():
     for i in tasks:
         print(i)
 
 print("your tasks today are: ")
+fillList()
+
+questionToAddTheTaskList = 0
+
+def userQuestions():
+    questionToAddTheTaskList = input('do you want to add any new tasks? ')
+
+    if questionToAddTheTaskList == 'yes' or questionToAddTheTaskList == 'yea' or questionToAddTheTaskList == 'yep':
+        inputAddedToTaskList = input('which task you want to add to the list ')
+        tasks.append(inputAddedToTaskList)
+
+    elif questionToAddTheTaskList == 'not' or questionToAddTheTaskList == 'no' or questionToAddTheTaskList == 'non' or questionToAddTheTaskList == 'nay' or questionToAddTheTaskList == 'nope':
+        print('ok')
+
+    else:
+        print('invalid response')
+
+
+def deleteTaks():
+    pass
+
+userQuestions()
+print('________')
 fillList()
